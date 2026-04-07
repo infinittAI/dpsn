03e83210c8775befe57ef3a91a29cb96f5ce70dc
jisung <jisung6723@gmail.com>
2026-04-07 19:21:12 +0900

    add load_patch

--- ai/wsi/loader.py
+++ ai/wsi/loader.py
@@ -1,5 +1,16 @@
+import numpy as np
+from openslide import OpenSlide
+
 from ai.wsi.patch import Patch
 from ai.wsi.patch_ref import PatchRef
 
 def load_patch(ref: PatchRef) -> Patch:
-    raise NotImplementedError
+    with OpenSlide(ref.image_path) as slide:
+        image = slide.read_region(ref.level0_path, ref.read_level, ref.read_size)
+    
+    array = np.array(image)
+
+    return Patch(
+        ref = ref,
+        img = array
+    )
