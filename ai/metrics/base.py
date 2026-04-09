ccfaf05538ec42126e53ad2b87bb9d63cbf6cc87
jisung <jisung6723@gmail.com>
2026-04-10 08:53:57 +0900

    ai/feat/add ssim metric

--- ai/metrics/base.py
+++ ai/metrics/base.py
@@ -1,3 +1,11 @@
+from abc import ABC, abstractmethod
 
-class Metric:
-    pass
+from ai.wsi.handle import WSIHandle
+
+class Metric(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def evaluate(self, origin_image: WSIHandle, normalized_image: WSIHandle) -> float:
+        pass
