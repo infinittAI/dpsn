5f6baff459d560bc919255d23e44ff0482c44a12
yebin <yebinp1926@gmail.com>
2026-04-27 15:16:52 +0900

    Feat: split requirements.txt for server, and change train_stainnet epoch

--- ai/models/stainnet/__init__.py
+++ ai/models/stainnet/__init__.py
@@ -1,8 +1,8 @@
 from ai.models.stainnet.paired_aligned_dataset import PairedAlignedImageDataset
 from ai.models.stainnet.stainnet_model import StainNet
-from ai.models.stainnet.train_stainnet import (
-    DEFAULT_APERIO_DIR,
-    DEFAULT_HAMAMATSU_DIR,
-    StainNetTrainingConfig,
-    train,
-)
+# from ai.models.stainnet.train_stainnet import (
+#     DEFAULT_APERIO_DIR,
+#     DEFAULT_HAMAMATSU_DIR,
+#     StainNetTrainingConfig,
+#     train,
+# )
